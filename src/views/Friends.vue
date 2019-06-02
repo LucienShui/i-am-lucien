ab2821179fff8b7a0851e0b09aaf92544f537824
LucienShui <lucien@lucien.ink>
2019-06-02 13:36:04 +0800

    update friends

--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -1,9 +1,9 @@
 <template>
     <main>
         <h1 id="friends">Friends</h1>
-        <p>Nothing yet. Please visit the source: <a
+        <p>暂未完工，可以到这里看看我的小伙伴们: <a
                 href="https://blog.lucien.ink/friends.html"
-                target="_blank">Friends in Lucien's Blog</a>.</p>
+                target="_blank">Lucien's Blog - 小伙伴们</a></p>
     </main>
 </template>
 
